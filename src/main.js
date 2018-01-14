1eca5f292202a3072e0a9268cd120538072a5254
Michał Budzyński <michal@virtualdesign.pl>
2018-01-14 01:40:00 +0100

    :blue_heart: init method

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
 const elements = [];
+const speed = 0.002;
+let scene;
 
-const scene = document.querySelector('#scene1');
 
 function add_element() {
 	const el = document.createElement('a-entity');
 	el.setAttribute('collada-model', '#rock1');
 	el.setAttribute('position', '0 .1 -4');
-
+	window.el = el;
 	scene.appendChild(el);
 }
 
+AFRAME.registerComponent('move', {
+	tick() {
+		this.el.object3D.position.z += speed;
+	}
+});
 
-add_element();
+
+function init() {
+	scene = document.querySelector('#scene1');
+	add_element();
+}
