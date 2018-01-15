646afa675e7575e5d6d37b97b842234dd795c5de
Michał Budzyński <michal@virtualdesign.pl>
2018-01-15 01:47:00 +0100

    :fallen_leaf: random scale

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ let scene;
 
 function add_element() {
 	const el = document.createElement('a-entity');
+	const scale = Math.random();
+
 	el.setAttribute('collada-model', '#rock1');
 	el.setAttribute('position', '0 .1 -4');
-	el.setAttribute('rotation', `${~~(Math.random()*360)} ${~~(Math.random()*360)} ${~~(Math.random()*360)}`);
+	el.setAttribute('scale', `${scale} ${Math.random()} ${Math.random()}`);
+	el.setAttribute('rotation', `0 0 ${~~(Math.random()*360)}`);
 	el.setAttribute('move', '');
 	window.el = el;
 	scene.appendChild(el);
