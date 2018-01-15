208a94ae17cfcac72cab14eacd2e8b142b71d3e2
Michał Budzyński <michal@virtualdesign.pl>
2018-01-15 01:57:00 +0100

    :handbag: position modifier

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ const speed = 0.00002;
 let scene;
 const road_size = 8;
 const max_distance = 30;
+const rocks = 10;
 
 function add_element() {
 	const el = document.createElement('a-entity');
 	const scale = 0.4 + Math.random();
+	const position_modifier = 1 - Math.random()*2;
 
 	el.setAttribute('collada-model', '#rock1');
-	el.setAttribute('position', `-4 .1 -${max_distance}`);
+	el.setAttribute('position', `-${road_size/2 + position_modifier} 0 -${max_distance}`);
 	el.setAttribute('scale', `${scale} ${scale} ${scale}`);
 	el.setAttribute('rotation', `0 ${~~(Math.random()*360)} 0`);
 	el.setAttribute('move', '');
