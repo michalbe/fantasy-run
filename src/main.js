bfc31ca3331f84b4b89d09c3e2f04074251e17b3
Michał Budzyński <michal@virtualdesign.pl>
2018-01-15 01:53:00 +0100

    :satellite: distance

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 const speed = 0.00002;
 let scene;
-
+const road_size = 8;
+const max_distance = 30;
 
 function add_element() {
 	const el = document.createElement('a-entity');
 	const scale = 0.4 + Math.random();
 
 	el.setAttribute('collada-model', '#rock1');
-	el.setAttribute('position', '0 .1 -4');
+	el.setAttribute('position', `-4 .1 -${max_distance}`);
 	el.setAttribute('scale', `${scale} ${scale} ${scale}`);
 	el.setAttribute('rotation', `0 ${~~(Math.random()*360)} 0`);
 	el.setAttribute('move', '');
