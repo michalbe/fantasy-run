0f628498f9bb0cb73035e64b048678aa87554972
Michał Budzyński <michal@virtualdesign.pl>
2018-01-21 02:29:00 +0100

    :golf: better rocks management

--- src/main.js
+++ src/main.js
@@ -1,30 +1,34 @@
-const speed = 0.02;
+const speed = 0.2;
 let scene;
-const road_size = 8;
-const max_distance = 30;
+const road_size = 12;
+const max_distance = 50;
 const rocks = 20;
 
-function add_rock(isLeft, zPos) {
-	const el = document.createElement('a-entity');
+function add_element(isLeft, zPos, element) {
+	const el = element || document.createElement('a-entity');
 	const scale = 0.4 + Math.random();
 	const position_modifier = 1 - Math.random()*2;
 	const multiplier = isLeft ? -1 : 1;
 
-	el.setAttribute('collada-model', '#rock1');
 	el.setAttribute('position', `${(road_size/2 + position_modifier) * multiplier} 0 ${zPos}`);
 	el.setAttribute('scale', `${scale} ${scale} ${scale}`);
 	el.setAttribute('rotation', `0 ${~~(Math.random()*360)} 0`);
-	el.setAttribute('move', '');
 
 	window.el = el;
-	scene.appendChild(el);
+	if (!element) {
+		el.setAttribute('collada-model', '#rock1');
+		el.setAttribute('move', '');
+		scene.appendChild(el);
+	}
 }
 
 AFRAME.registerComponent('move', {
 	tick() {
 		const position_modifier = 1 - Math.random()*2;
 		if (this.el.object3D.position.z > max_distance) {
-			this.el.object3D.position.z = -max_distance + position_modifier;
+			add_element(Math.random() > 0.5, - max_distance, this.el);
+			// this.el.object3D.position.z = -max_distance + position_modifier;
+
 		}
 		this.el.object3D.position.z += speed;
 	}
@@ -34,6 +38,6 @@ function init() {
 	scene = document.querySelector('#scene1');
 	const step = (max_distance * 2) / rocks;
 	for (let i = 0; i < rocks; i++) {
-		add_rock(Math.random() > 0.5, i * step - max_distance);
+		add_element(Math.random() > 0.5, i * step - max_distance);
 	}
 }
