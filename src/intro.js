9e1a0069e9d10c1083012d7073acc02f637fca84
Michał Budzyński <michal@virtualdesign.pl>
2018-02-12 01:25:00 +0100

    :raised_hand: namespace main game thingy

--- src/intro.js
+++ src/intro.js
@@ -2,8 +2,7 @@ function go_to_github() {
 	window.open('http://github.com/michalbe');
 }
 
-
-function init() {
+function intro_init() {
 	const points_element = document.querySelector('#points');
 	const points = window.localStorage.getItem('points') || 0;
 	points_element.setAttribute('value', `Best: ${points}m`);
